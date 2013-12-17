d53514b865d411e660e0ddfc01fedb5fb3e09a1e
Ronen Shraga <mobiletecinternationalinc@Ronens-iMac.local>
2013-12-17 22:08:25 +0200

    Initial Commit

---
+++ ViGuard/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ViGuard
+//
+//  Created by Ronen Shraga on 12/17/13.
+//  Copyright (c) 2013 Vitalitix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
